c7a19b3a3b2eb62ff3c24662a6901b3c7c470aef
Fiona Lapham <fionadlapham@gmail.com>
2018-05-29 10:43:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,9 +63,8 @@ class Dog
      new_dog = Dog.new(name: dog_data[1], breed: dog_data[2], id: dog_data[0])
      binding.pry
    else
-     dog = self.create(name: name, breed: breed)
+     created_dog = self.create(name: name, breed: breed)
    end
-   dog
  end
 
 
